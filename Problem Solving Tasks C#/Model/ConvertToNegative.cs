6c34be07c06318e146c955b3333c62f0b4e38612
Amr Talaat <amrt2598@gmail.com>
2025-10-05 11:59:44 +0300

    Convert To Negative

---
+++ Problem Solving Tasks C#/Model/ConvertToNegative.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Solving_Tasks_C_.Model;
+public class ConvertToNegative
+{
+    public int MakeNegative(int number)
+    {
+        if (number > 0)
+            return number * -1;
+
+        else
+            return number;
+
+    }
+}
--- Problem Solving Tasks C#/Program.cs
+++ Problem Solving Tasks C#/Program.cs
@@ -1,12 +1,14 @@
 ﻿using Problem_Solving_Tasks_C_.Class;
+using Problem_Solving_Tasks_C_.Model;
 using System;
 internal class Program
 {
     private static void Main(string[] args)
     {
         ConvertNumToString converter = new ConvertNumToString();
+        ConvertToNegative  negative = new ConvertToNegative();
 
         int num = 5;
-        Console.WriteLine(converter.NumberToString(num));
+        Console.WriteLine(negative.MakeNegative(num));
     }
 }
