3ac1a44527af48ea9a620d123b1214eef3c4c2c2
Amr Talaat <amrt2598@gmail.com>
2025-10-06 11:49:18 +0300

    Double Char in String

---
+++ Problem Solving Tasks C#/Model/DoubleChar.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem_Solving_Tasks_C_.Model;
+internal class DoubleChar
+{
+    public string DoubleString(string str)
+    {
+        char[] chars = str.ToCharArray();
+        var list = new List<string>();  
+        for(int i  = 0; i < chars.Length; i++)
+        {
+            list.Add(char.ToString(chars[i]));
+            list.Add(char.ToString(chars[i]));
+        }
+        return string.Join("", list.ToArray());
+    }
+
+}
--- Problem Solving Tasks C#/Program.cs
+++ Problem Solving Tasks C#/Program.cs
@@ -6,10 +6,12 @@ internal class Program
     private static void Main(string[] args)
     {
         ReverseString reverseString = new ReverseString();
+        DoubleChar doubleChar = new DoubleChar();
 
-        string str = "amrtalaat ";
+        string str = "amr talaat ";
         Console.WriteLine(reverseString.ReverseOne(str));
         Console.WriteLine(reverseString.ReverseTow(str));
+        Console.WriteLine(doubleChar.DoubleString(str));
 
 
 
