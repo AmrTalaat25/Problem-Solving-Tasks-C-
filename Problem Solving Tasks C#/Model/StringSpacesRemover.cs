017ce879e72910a970b54f893a0afd01edee76cf
Amr Talaat <amrt2598@gmail.com>
2025-10-09 11:36:04 +0300

    Remove String Spaces

---
+++ Problem Solving Tasks C#/Model/StringSpacesRemover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Solving_Tasks_C_.Model;
+internal class StringSpacesRemover
+{
+    public  string NoSpace(string input)
+    {
+        var newString = input.Split(" ");
+        return string.Join("",newString);
+    }
+}
--- Problem Solving Tasks C#/Program.cs
+++ Problem Solving Tasks C#/Program.cs
@@ -6,8 +6,11 @@ internal class Program
     private static void Main(string[] args)
     {
         RemoveTimeFromDate removeTimeFromDate = new RemoveTimeFromDate();
+        StringSpacesRemover stringSpacesRemover = new StringSpacesRemover();
         string lomgtime = "Friday May 2, 7pm";
+        string anyString = "8 j 8   mBliB8g  imjB8B8  jl  B";
         Console.WriteLine(removeTimeFromDate.ShortenToDate(lomgtime));
+        Console.WriteLine(stringSpacesRemover.NoSpace(anyString));
 
     }
 }
