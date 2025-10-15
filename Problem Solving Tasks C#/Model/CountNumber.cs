5a900ce3624f663a3624c04ac00abbfe4fb8f0bf
Amr Talaat <amrt2598@gmail.com>
2025-10-15 13:48:34 +0300

    Count Even and Odd Numbers in an Array

---
+++ Problem Solving Tasks C#/Model/CountNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Solving_Tasks_C_.Model;
+internal class CountNumber
+{
+    public void count(int[] Numbers)
+    {
+        int countEven = 0;
+        int countOdd = 0;
+        foreach (int number in Numbers) 
+            {
+                if (number%2 == 0)
+                    countEven++; 
+                else
+                    countOdd ++;
+
+            }
+        Console.WriteLine($"Count Even Number Is : {countEven}");
+        Console.WriteLine($"Count Odd Number Is : {countOdd}");
+    }
+}
--- Problem Solving Tasks C#/Program.cs
+++ Problem Solving Tasks C#/Program.cs
@@ -6,9 +6,12 @@ internal class Program
     private static void Main(string[] args)
     {
         SwapTwoNumbers swapTwoNumbers = new SwapTwoNumbers();
+        CountNumber countNumber = new CountNumber();
+        int[] numbers = { 1, 5 , 6 , 4 , 7, 8 ,10 ,11 ,44};
         int x = 3;
         int y = 5;
         swapTwoNumbers.Swap( x,  y);
+        countNumber.count(numbers);
 
 
 
