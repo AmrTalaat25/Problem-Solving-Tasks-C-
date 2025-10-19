ae7be052e51db9055c55b9d15301505c43deb65d
Amr Talaat <amrt2598@gmail.com>
2025-10-19 12:57:30 +0300

    Avarage In Array

---
+++ Problem Solving Tasks C#/Model/AvarageInArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Solving_Tasks_C_.Model;
+internal class AvarageInArray
+{
+    public double FindAverage(double[] array)
+    {
+        double sum = 0;
+
+        for (int i = 0; i < array.Length; i++) 
+        {
+            sum += array[i];
+        
+        }
+        
+        return sum / array.Length;
+    }
+
+}
--- Problem Solving Tasks C#/Program.cs
+++ Problem Solving Tasks C#/Program.cs
@@ -7,12 +7,15 @@ internal class Program
     {
         StringIsUpper stringIsUpper = new StringIsUpper();
         RemoveCharFromString RemoveCharFromString = new RemoveCharFromString();
+        AvarageInArray averageArray = new AvarageInArray();
         string text = "hello I AM AMR"; 
         string Ntext = "HELLO I AM AMR"; 
+        double[] doubles = {1, 2, 3, 4, 5}; 
 
         Console.WriteLine(stringIsUpper.IsUpperCase(text));
         Console.WriteLine(stringIsUpper.IsUpperCase(Ntext));
         Console.WriteLine(RemoveCharFromString.Remove_char(text));
+        Console.WriteLine($"The Average Of Array : {averageArray.FindAverage(doubles)}");
 
 
 
